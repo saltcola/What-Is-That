d8a05bace6bcc4f7b72c51e63a96e76d7a6dc199
Gang Fan <gang.fan@uconn.edu>
2016-03-31 10:27:55 -0400

    Modify style

--- client/main.js
+++ client/main.js
@@ -1,37 +1,19 @@
-/*
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
 
-import './main.html';
-
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
-
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
-
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
-});
-
-Template.hello.greeting = function () {
-	return "Welcome to forum.";
-};*/
-
-var Posts = new Meteor.Collection('posts');
-
-if (Meteor.isClient) {
-  Template.posts.helpers({
-    posts: function() {
-      return Posts.find();
+Template.example.events({
+    'click .takePhoto': function(event, template) {
+        var cameraOptions = {
+            width: 800,
+            height: 600
+        };
+        MeteorCamera.getPicture(cameraOptions, function (error, data) {
+           if (!error) {
+               template.$('.photo').attr('src', data); 
+           }
+        });
+        event.preventDefault();
     }
-  })
-}
+});
+
+
+
+
