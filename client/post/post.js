31084cfcf024c49fe372c03d21e07d034f97b50c
Gang Fan <gang.fan@uconn.edu>
2016-04-28 15:01:31 -0400

    Add page to show user own posts

--- client/post/post.js
+++ client/post/post.js
@@ -3,6 +3,23 @@
         posts: function() {
             return Posts.find();
         },
+
+       image: function() {
+      var image = PostImages.findOne({associatedObjectId: this._id});
+      if (image) {
+        return image.url({store: "postImages-normal"});
+      }
+    }
+
+    });
+
+    Template.userPosts.helpers({
+        posts: function() {
+            username = Meteor.user().username
+            console.log(username)
+            return Posts.find({'username':username});
+        },
+
        image: function() {
       var image = PostImages.findOne({associatedObjectId: this._id});
       if (image) {
