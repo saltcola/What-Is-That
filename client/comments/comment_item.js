049a4f0dc7b27af589cc2190dc116ad231240e39
Gang Fan <gang.fan@uconn.edu>
2016-05-12 11:05:07 -0400

    Modify background color

--- client/comments/comment_item.js
+++ client/comments/comment_item.js
@@ -1,5 +1,14 @@
 Template.commentItem.helpers({
   submittedText: function() {
     return this.submitted.toString();
+  },
+  ownComments:function(){
+  	let postId = this.postId;
+  	let username= Posts.findOne({"_id":postId}).username;
+  	if (username == this.commentBy){
+  		return true;
+  	}
+  	return false;
+
   }
 });
